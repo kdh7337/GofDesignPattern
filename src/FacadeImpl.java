e8ec52d7b07801f0ccbbe445bcef0552c7298bdd
DoDo <kdh7337@gmail.com>
2017-02-10 16:26:27 +0900

    Facade Pattern Upload

---
+++ src/FacadeImpl.java
@@ -0,0 +1,10 @@
+/**
+ * Created by DoDo on 2017-02-10.
+ */
+public class FacadeImpl {
+
+    public void facadeExample(){
+        FacadePattern facade = new FacadePattern();
+        facade.doComplexSimply();
+    }
+}
---
+++ src/FacadePattern.java
@@ -0,0 +1,20 @@
+/**
+ * Created by DoDo on 2017-02-10.
+ */
+
+public class FacadePattern {
+
+        private ComponentA componentA = new ComponentA();
+        private ComponentB componentB = new ComponentB();
+
+        public void doComplexSimply(){
+            //복잡한 요소간의 상호 작용
+        }
+
+    private class ComponentA {
+        //다양한 일
+    }
+    private class ComponentB{
+        //다양한 일
+    }
+}
